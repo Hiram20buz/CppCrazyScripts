2c3dda8ac27b2cd96199a718837f0c6a5a1cb609
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-19 21:44:44 -0700

    Create declareVarInIf.cpp

---
+++ declareVarInIf.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+
+    if (int c = 10; c % 2 == 0) { 
+      // Declare and initialize 'c' inside the if block
+        std::cout << "Hello world!";
+    }
+
+    return 0;
+}
