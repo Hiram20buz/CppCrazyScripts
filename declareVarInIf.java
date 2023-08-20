f3e0a9908ae18c03c4408944da5fdf25588cc572
Hiram20buz <112133798+Hiram20buz@users.noreply.github.com>
2023-08-19 23:06:43 -0700

    Create declareVarInIf.java

---
+++ declareVarInIf.java
@@ -0,0 +1,9 @@
+public class declareVarInIf {
+    public static void main(String[] args) {
+        int c = 10;
+
+        if (c % 2 == 0) {
+            System.out.println("Hello world");
+        }
+    }
+}
